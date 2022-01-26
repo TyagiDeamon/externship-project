97d80ecc6e659f54ce02c1fb6ea22f0c30d17f02
Karan Tyagi <karantyagi9702@gmail.com>
2022-01-26 20:45:48 +0530

    Set deleteComment route

--- server/routes/postRoutes.js
+++ server/routes/postRoutes.js
@@ -4,6 +4,7 @@ import createPost from "../controllers/post/createPost.js";
 import likePost from "../controllers/post/likePost.js";
 import unlikePost from "../controllers/post/unlikePost.js";
 import addComment from "../controllers/post/addComment.js";
+import deleteComment from "../controllers/post/deleteComment.js";
 
 import {
 	getUser,
@@ -22,5 +23,7 @@ router.patch("/like/:id", verifyLogin, likePost);
 router.patch("/unlike/:id", verifyLogin, unlikePost);
 
 router.patch("/addComment/:id", verifyLogin, addComment);
+
+router.patch("/deleteComment/:id", verifyLogin, deleteComment);
 
 export default router;
